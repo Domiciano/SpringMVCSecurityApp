31fb0318a9fa55b5b558e39dc45a9f7371a1c98c
Domiciano Rincón <domicianorincon@Domicianos-MacBook-Pro.local>
2025-08-28 15:24:40 -0500

    Semana 5

---
+++ src/main/java/org/example/introspringboot/entity/Student.java
@@ -0,0 +1,56 @@
+package org.example.introspringboot.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String code;
+    private String program;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, String code, String program) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.program = program;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+}
