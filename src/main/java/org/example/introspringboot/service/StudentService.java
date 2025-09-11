3a3fd0f56079b35768069b1a12f72ca4fe2708fe
Domiciano Rincón <domicianorincon@Domicianos-MacBook-Pro.local>
2025-09-11 16:02:28 -0500

    Semana 7

--- src/main/java/org/example/introspringboot/service/StudentService.java
+++ src/main/java/org/example/introspringboot/service/StudentService.java
@@ -3,6 +3,7 @@ package org.example.introspringboot.service;
 import org.example.introspringboot.entity.Student;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentService {
 
@@ -10,4 +11,7 @@ public interface StudentService {
 
     List<Student> getStudents();
 
+    Optional<Student> findStudentByCode(String code);
+
+    List<Student> getStudentsByCourseName(String name);
 }
